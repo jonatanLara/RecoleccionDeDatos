62d6f514fe437e925be1956749897e80796dbcf9
jonatanlara <jonatan2332@hotmail.com>
2022-09-19 07:11:39 -0500

    AJAX

---
+++ models/ajax.php
@@ -0,0 +1,17 @@
+<?php
+ require_once "../../controllers/controller.php";
+ require_once "../../models/crud.php";
+
+ class Ajax{
+   public $validarUsuario;
+   public function validarUsuarioAjax(){
+     $datos = $$this->validarUsuario;
+  #   $respuesta = MvcController::validarUsuarioController($datos);
+  #   echo $respuesta;
+    echo $datos;
+   }
+ }
+ $a = new Ajax();
+ $a-> validarUsuario = $_POST[""];
+ $a-> validarUsuarioAjax();
+ ?>
--- models/crud.php
+++ models/crud.php
@@ -150,4 +150,13 @@ class Datos extends Conexion{
         }
         $stmt->close();
     }
+    #Consulta con Ajax
+    public static function validarUsuarioModel(){
+      $stmt = Conexion::conectar()->prepare("SELECT bd_tm_pro_id, bd_tm_pro_nombre, bd_tm_pro_apellido_p, bd_tm_pro_apellido_m,
+        bd_tm_pro_id_estatus, bd_tm_pro_id_usuario, bd_tm_pro_matricula, bd_tm_pro_pass FROM $tabla WHERE bd_tm_pro_matricula = :clave_matricula");
+      $stmt->bindParam(":clave_matricula", $datosModels["clave_matricula"],PDO::PARAM_STR);
+      $stmt->execute();
+      return $stmt->fetch();
+      $stmt->close();
+    }
 }
